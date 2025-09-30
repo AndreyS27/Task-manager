5c269bc00f5f7427893cc01d28047a0c91b700fb
Andrey S <salihov.andrey1@yandex.ru>
2025-09-30 13:05:25 +0300

    feat: add User and TodoItem models with DbContext

---
+++ TaskManager.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManager.Api.Models;
+
+namespace TaskManager.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<TodoItem> TodoItems => Set<TodoItem>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            modelBuilder.Entity<TodoItem>()
+                .HasOne(t => t.User)
+                .WithMany()
+                .HasForeignKey(t => t.UserId);
+        }
+    }
+}
