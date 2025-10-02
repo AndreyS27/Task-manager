13f26fdbd66337f7f36f0679cf58224edb75ca1a
Andrey S <salihov.andrey1@yandex.ru>
2025-10-02 11:59:08 +0300

    feat: implement auth controller

---
+++ TaskManager.Api/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Api.DTOs;
+using TaskManager.Api.Services;
+
+namespace TaskManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthResponseDto>> Register(RegisterDto dto)
+        {
+            try
+            {
+                var response = await _authService.RegisterAsync(dto);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponseDto>> Login(LoginDto dto)
+        {
+            try
+            {
+                var response = await _authService.LoginAsync(dto);
+                return Ok(response);
+            }
+            catch (Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+    }
+}
