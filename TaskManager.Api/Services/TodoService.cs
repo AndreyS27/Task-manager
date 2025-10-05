61244f03c92c9c6cc3138c1f8109bdbf8459a3ec
Andrey S <salihov.andrey1@yandex.ru>
2025-10-05 13:41:50 +0300

    feat: add TodoService

---
+++ TaskManager.Api/Services/TodoService.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Api.Data;
+using TaskManager.Api.DTOs;
+using TaskManager.Api.Models;
+
+namespace TaskManager.Api.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<TodoItemDto> CreateAsync(CreateTodoDto dto, int userId)
+        {
+            var todoItem = new TodoItem
+            {
+                Title = dto.Title,
+                Description = dto.Description,
+                UserId = userId
+            };
+
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+
+            return new TodoItemDto 
+            { 
+                Id = todoItem.Id, 
+                Title = todoItem.Title,
+                Description = todoItem.Description,
+                IsCompleted = todoItem.IsCompleted,
+                CreatedAt = todoItem.CreatedAt
+            };
+        }
+
+        public async Task DeleteAsync(int todoId, int userId)
+        {
+            var todoItem = await _context.TodoItems
+                .FirstOrDefaultAsync(t => t.Id == todoId && t.UserId == userId);
+
+            if (todoItem == null)
+            {
+                throw new Exception("Task not found or access denied");
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<TodoItemDto>> GetByUserIdAsync(int userId)
+        {
+            return await _context.TodoItems
+                .Where(t => t.UserId == userId)
+                .Select(t => new TodoItemDto
+                {
+                    Id = t.Id,
+                    Title = t.Title,
+                    Description = t.Description,
+                    IsCompleted = t.IsCompleted,
+                    CreatedAt = t.CreatedAt,
+                })
+                .ToListAsync();
+        }
+
+        public async Task<TodoItemDto> UpdateAsync(int todoId, UpdateTodoDto dto, int userId)
+        {
+            var todoItem = await _context.TodoItems
+                .FirstOrDefaultAsync(t => t.Id == todoId && t.UserId == userId);
+
+            if (todoItem == null)
+            {
+                throw new Exception("Task not found or access denied");
+            }
+
+            if (!String.IsNullOrEmpty(dto.Title))
+            {
+                todoItem.Title = dto.Title;
+            }
+
+            if (dto.Description != null)
+            {
+                todoItem.Description = dto.Description;
+            }
+
+            if (dto.IsCompleted.HasValue) 
+            {
+                todoItem.IsCompleted = dto.IsCompleted.Value;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return new TodoItemDto
+            {
+                Id = todoItem.Id,
+                Title = todoItem.Title,
+                Description = todoItem.Description,
+                IsCompleted = todoItem.IsCompleted,
+                CreatedAt = todoItem.CreatedAt
+            };
+        }
+    }
+}
